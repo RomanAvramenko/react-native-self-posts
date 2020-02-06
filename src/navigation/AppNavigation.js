8eead4c02917a5cbd017730bd3ac27ef075809da
unknown <ashkaell24@gmail.com>
2020-02-06 19:25:24 +0200

    first commit, new app

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,22 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { MainScreen } from '../screens/MainScreen'
+import { PostScreen } from '../screens/PostScreen'
+import { THEME } from '../theme'
+
+const PostNavigator = createStackNavigator({
+    Main: MainScreen,
+    Post: {
+        screen: PostScreen
+    }
+}, {
+    initialRouteName: 'Main',
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: THEME.MAIN_COLOR
+        },
+        headerTintColor: '#ffffff'
+    }
+})
+
+export const AppNavigation = createAppContainer(PostNavigator)
---
+++ src/screens/PostScreen.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, Button, ScrollView, Alert } from 'react-native'
+import { DATA } from '../data'
+import { THEME } from '../theme'
+
+export const PostScreen = ({ navigation }) => {
+  const postId = navigation.getParam('postId')
+
+  const post = DATA.find(p => p.id === postId)
+
+  const removeHandler = () => {
+    Alert.alert(
+      'Deleting Post',
+      'Are you sure?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        { text: 'Delete', style: 'destructive', onPress: () => { } },
+      ],
+      { cancelable: false },
+    );
+  }
+
+  return (
+    <ScrollView>
+      <Image source={{ uri: post.img }} style={styles.image} />
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button title='Delete' color={THEME.DANGER_COLOR} onPress={removeHandler} />
+    </ScrollView>
+  )
+}
+
+PostScreen.navigationOptions = ({ navigation }) => {
+  const date = navigation.getParam('date')
+
+  return {
+    headerTitle: 'Post from ' + new Date(date).toLocaleDateString()
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200
+  },
+  textWrap: {
+    padding: 10
+  },
+  title: {
+    fontFamily: 'open-regular'
+  }
+})
